3c294d45908237518b0de4015a947dfc863b6fbd
Blondel MONDESIR <blondel.md@gmail.com>
2025-11-12 19:20:29 -0500

    refactor(rapidpro): Use gunicorn's forwarded-allow-ips
     - Use gunicorn's '--forwarded-allow-ips' option to handle the 'X-Forwarded-For' header.
    - This is a more robust solution than the custom middleware.

--- roles/rapidpro/files/x_forwarded_for_middleware.py
+++
@@ -1,9 +0,0 @@
-class XForwardedForMiddleware:
-    def __init__(self, get_response):
-        self.get_response = get_response
-
-    def __call__(self, request):
-        if 'HTTP_X_FORWARDED_FOR' in request.META:
-            request.META['REMOTE_ADDR'] = request.META['HTTP_X_FORWARDED_FOR'].split(',')[0].strip()
-        response = self.get_response(request)
-        return response
